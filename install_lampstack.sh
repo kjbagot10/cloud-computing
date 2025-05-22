b3063b4f30cf0730a255373cca6a67ebf1d9cdaa
kjbagot10 <151742506+kjbagot10@users.noreply.github.com>
2025-05-22 11:48:09 +0100

    Update install_lampstack.sh

--- install_lampstack.sh
+++ install_lampstack.sh
@@ -7,15 +7,15 @@
 sudo su
 add-apt-repository ppa:ondrej/php -y
 apt-get update
-apt-get install php8.1 php8.1-dev php8.1-xml -y --allow-unauthenticated
+apt-get install php8.3 php8.3-dev php8.3-xml -y --allow-unauthenticated
 
 # install prequisites
 sudo apt-get install unixodbc-dev
 sudo pecl install sqlsrv
 sudo pecl install pdo_sqlsrv
 sudo su
-printf "; priority=20\nextension=sqlsrv.so\n" > /etc/php/8.1/mods-available/sqlsrv.ini
-printf "; priority=30\nextension=pdo_sqlsrv.so\n" > /etc/php/8.1/mods-available/pdo_sqlsrv.ini
+printf "; priority=20\nextension=sqlsrv.so\n" > /etc/php/8.3/mods-available/sqlsrv.ini
+printf "; priority=30\nextension=pdo_sqlsrv.so\n" > /etc/php/8.3/mods-available/pdo_sqlsrv.ini
 exit
 sudo phpenmod -v 8.1 sqlsrv pdo_sqlsrv
 
@@ -23,17 +23,17 @@ sudo phpenmod -v 8.1 sqlsrv pdo_sqlsrv
 sudo pecl install sqlsrv
 sudo pecl install pdo_sqlsrv
 sudo su
-printf "; priority=20\nextension=sqlsrv.so\n" > /etc/php/8.1/mods-available/sqlsrv.ini
-printf "; priority=30\nextension=pdo_sqlsrv.so\n" > /etc/php/8.1/mods-available/pdo_sqlsrv.ini
+printf "; priority=20\nextension=sqlsrv.so\n" > /etc/php/8.3/mods-available/sqlsrv.ini
+printf "; priority=30\nextension=pdo_sqlsrv.so\n" > /etc/php/8.3/mods-available/pdo_sqlsrv.ini
 exit
-sudo phpenmod -v 8.1 sqlsrv pdo_sqlsrv
+sudo phpenmod -v 8.3 sqlsrv pdo_sqlsrv
 
 # install apache
 sudo su
-apt-get install libapache2-mod-php8.1 apache2
+apt-get install libapache2-mod-php8.3 apache2
 a2dismod mpm_event
 a2enmod mpm_prefork
-a2enmod php8.1
+a2enmod php8.3
 exit
 
 sudo service apache2 restart
