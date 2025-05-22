3575bad9444c26cc3cb4ddffe00d647bbb94bed8
kjbagot10 <151742506+kjbagot10@users.noreply.github.com>
2025-05-22 15:51:47 +0100

    Update install_lampstack.sh

--- install_lampstack.sh
+++ install_lampstack.sh
@@ -26,10 +26,9 @@ sudo apt-get install -y unixodbc-dev
 
 # https://learn.microsoft.com/en-us/sql/connect/php/installation-tutorial-linux-mac?view=sql-server-ver16#testing-your-installation
 # isntall php
-sudo su
-add-apt-repository ppa:ondrej/php -y
-apt-get update
-apt-get install php8.3 php8.3-dev php8.3-xml -y --allow-unauthenticated
+sudo add-apt-repository ppa:ondrej/php -y
+sudo apt-get update
+sudo apt-get install php8.3 php8.3-dev php8.3-xml -y --allow-unauthenticated
 
 # install prequisites
 sudo apt-get install unixodbc-dev
@@ -38,16 +37,15 @@ sudo pecl install pdo_sqlsrv
 sudo su
 printf "; priority=20\nextension=sqlsrv.so\n" > /etc/php/8.3/mods-available/sqlsrv.ini
 printf "; priority=30\nextension=pdo_sqlsrv.so\n" > /etc/php/8.3/mods-available/pdo_sqlsrv.ini
-exit
+
 sudo phpenmod -v 8.3 sqlsrv pdo_sqlsrv
 
 # install apache
-sudo su
-apt-get install libapache2-mod-php8.3 apache2
-a2dismod mpm_event
-a2enmod mpm_prefork
-a2enmod php8.3
-exit
+sudo apt-get install libapache2-mod-php8.3 apache2
+sudo a2dismod mpm_event
+sudo a2enmod mpm_prefork
+sudo a2enmod php8.3
+
 
 sudo service apache2 restart
 
