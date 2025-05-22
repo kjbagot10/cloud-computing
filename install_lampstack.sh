05b4872cfb6345b7f8df3340ccce3141b92647c4
kjbagot10 <151742506+kjbagot10@users.noreply.github.com>
2025-05-22 15:26:11 +0100

    Update install_lampstack.sh

--- install_lampstack.sh
+++ install_lampstack.sh
@@ -36,10 +36,10 @@ sudo apt-get install unixodbc-dev
 sudo pecl install sqlsrv
 sudo pecl install pdo_sqlsrv
 sudo su
-printf "; priority=20\nextension=sqlsrv.so\n" > /etc/php/8.3/mods-available/sqlsrv.ini
-printf "; priority=30\nextension=pdo_sqlsrv.so\n" > /etc/php/8.3/mods-available/pdo_sqlsrv.ini
+printf "; priority=20\nextension=sqlsrv.so\n" > /etc/php/8.1/mods-available/sqlsrv.ini
+printf "; priority=30\nextension=pdo_sqlsrv.so\n" > /etc/php/8.1/mods-available/pdo_sqlsrv.ini
 exit
-sudo phpenmod -v 8.3 sqlsrv pdo_sqlsrv
+sudo phpenmod -v 8.1 sqlsrv pdo_sqlsrv
 
 # install apache
 sudo su
