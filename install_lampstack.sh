0b221bcf146cddf0349255ad2590c85619302cac
kjbagot10 <151742506+kjbagot10@users.noreply.github.com>
2025-05-22 16:45:58 +0100

    Update install_lampstack.sh

--- install_lampstack.sh
+++ install_lampstack.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+# 1. install php 
+sudo add-apt-repository ppa:ondrej/php -y
+sudo apt-get update
+sudo apt-get install php8.3 php8.3-dev php8.3-xml -y --allow-unauthenticated
 #https://learn.microsoft.com/en-us/sql/connect/odbc/linux-mac/installing-the-microsoft-odbc-driver-for-sql-server?view=sql-server-ver16&tabs=ubuntu18-install%2Calpine17-install%2Cdebian8-install%2Credhat7-13-install%2Crhel7-offline
-#installing the microsoft ODBC driver
 if ! [[ "20.04 22.04 24.04 24.10" == *"$(grep VERSION_ID /etc/os-release | cut -d '"' -f 2)"* ]];
 then
     echo "Ubuntu $(grep VERSION_ID /etc/os-release | cut -d '"' -f 2) is not currently supported.";
     exit;
 fi
-
 # Download the package to configure the Microsoft repo
 curl -sSL -O https://packages.microsoft.com/config/ubuntu/$(grep VERSION_ID /etc/os-release | cut -d '"' -f 2)/packages-microsoft-prod.deb
 # Install the package
 sudo dpkg -i packages-microsoft-prod.deb
 # Delete the file
 rm packages-microsoft-prod.deb
-
 # Install the driver
 sudo apt-get update
 sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
@@ -23,27 +24,17 @@ echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
 source ~/.bashrc
 # optional: for unixODBC development headers
 sudo apt-get install -y unixodbc-dev
-
 # https://learn.microsoft.com/en-us/sql/connect/php/installation-tutorial-linux-mac?view=sql-server-ver16#testing-your-installation
-# isntall php
-sudo add-apt-repository ppa:ondrej/php -y
-sudo apt-get update
-sudo apt-get install php8.3 php8.3-dev php8.3-xml -y --allow-unauthenticated
-
-# install prequisites
-sudo apt-get install unixodbc-dev
 sudo pecl install sqlsrv
 sudo pecl install pdo_sqlsrv
-printf "; priority=20\nextension=sqlsrv.so\n" > /etc/php/8.3/mods-available/sqlsrv.ini
-printf "; priority=30\nextension=pdo_sqlsrv.so\n" > /etc/php/8.3/mods-available/pdo_sqlsrv.ini
+printf "; priority=20\nextension=sqlsrv.so\n" > /etc/php/8.1/mods-available/sqlsrv.ini
+printf "; priority=30\nextension=pdo_sqlsrv.so\n" > /etc/php/8.1/mods-available/pdo_sqlsrv.ini
 
 sudo phpenmod -v 8.3 sqlsrv pdo_sqlsrv
-
-# install apache
 sudo apt-get install libapache2-mod-php8.3 apache2
 sudo a2dismod mpm_event
 sudo a2enmod mpm_prefork
-sudo a2enmod php8.3
+sudo a2enmod php8.1
 
 
 sudo service apache2 restart
