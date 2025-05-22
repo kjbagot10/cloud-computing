6f1fbf7f153017a14942865b60a78dbc38172c9f
kjbagot10 <151742506+kjbagot10@users.noreply.github.com>
2025-05-22 15:36:25 +0100

    Update install_lampstack.sh

--- install_lampstack.sh
+++ install_lampstack.sh
@@ -39,7 +39,7 @@ sudo su
 printf "; priority=20\nextension=sqlsrv.so\n" > /etc/php/8.3/mods-available/sqlsrv.ini
 printf "; priority=30\nextension=pdo_sqlsrv.so\n" > /etc/php/8.3/mods-available/pdo_sqlsrv.ini
 exit
-sudo phpenmod -v 8.1 sqlsrv pdo_sqlsrv
+sudo phpenmod -v 8.3 sqlsrv pdo_sqlsrv
 
 # install apache
 sudo su
